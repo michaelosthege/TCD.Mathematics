2e9b423f81b146d29a0247666e862e6e39209319
michaelosthege <thecakedev@hotmail.com>
2013-12-06 22:52:46 +0100

    WPF sample

---
+++ TCD.Mathematics.Sample.WPF/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using TCD.Mathematics;//get from NuGet
+
+namespace TCD.Mathematics.Sample.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            //do some calculations
+            Point3D p1 = new Point3D(3, 2, 1);
+            Line3D l1 = new Line3D(new Point3D(0, 0, 2), new Vector3D(1, 0, 0));
+            Plane3D e1 = new Plane3D(l1, new Point3D(1, 0, 0));
+
+            Point3D p2 = p1.ProjectOnLine3D(l1);
+            Point3D p3 = p1.ProjectOnPlane3D(e1);
+
+            console.Text += "\nPoint1                : " + p1;
+            console.Text += "\nLine1                 : " + l1;
+            console.Text += "\nPlane1                : " + e1;
+            console.Text += "\nPoint1 -> Line1       : " + p2;
+            console.Text += "\nPoint1 -> Plane1      : " + p3;
+
+            console.Text += "\n\nLine(Point2, Point3)  : " + new Line3D(p2, p3);
+
+            Line3D il = new Line3D(new Point3D(1, 1, 2), new Point3D(7, 5, 3));
+            Plane3D ip1 = new Plane3D(il, new Point3D(5, 5, 5));
+            Plane3D ip2 = new Plane3D(il, new Point3D(5, -5, 5));
+
+            Line3D il2 = Plane3D.IntersectPlane3Ds(ip1, ip2);
+
+            console.Text += "\n\nLine1                 : " + il + "\nLine1 (reconstructed) : " + il2;
+
+
+        }
+    }
+}
